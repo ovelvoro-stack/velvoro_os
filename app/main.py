3dc728829865adbc1b44814edb39984322a7adc8
ovelvoro-stack <ovelvoro@gmail.com>
2026-01-28 00:41:45 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,14 +3,12 @@ from app.models.api.daily_summary_routes import router as daily_summary_router
 
 app = FastAPI(title="Velvoro Daily OS")
 
+@app.get("/")
+def root():
+    return {"status": "Velvoro Daily OS running"}
+
 app.include_router(
     daily_summary_router,
     prefix="/daily-summary",
     tags=["Daily Summary"]
 )
-
-@app.get("/")
-def root():
-    return {
-        "status": "Velvoro Daily OS running"
-    }
