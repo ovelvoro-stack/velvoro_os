5d8d0a93fed077712723dc8d582c767d7a292c27
ovelvoro-stack <ovelvoro@gmail.com>
2026-01-28 21:55:08 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
-from fastapi.responses import RedirectResponse
-
-from app.auth.routes import auth_router
+from app.auth.routes import router as auth_router
+from app.frontend.routes import router as ui_router
+from app.billing.routes import router as billing_router
+from app.core.auth_middleware import AuthMiddleware
 
 app = FastAPI()
 
-@app.get("/", include_in_schema=False)
-def root():
-    return RedirectResponse(url="/login")
-
+app.add_middleware(AuthMiddleware)
 app.include_router(auth_router)
+app.include_router(ui_router)
+app.include_router(billing_router)
