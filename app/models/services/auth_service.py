faeea4235da33adf4b939a64916d9b118340adec
ovelvoro-stack <ovelvoro@gmail.com>
2026-01-28 18:20:37 +0530

    Update auth_service.py

--- app/models/services/auth_service.py
+++ app/models/services/auth_service.py
@@ -1,26 +1,22 @@
 import pandas as pd
-import os
 
-USERS_FILE = "data/users.xlsx"
+USERS = "data/users.xlsx"
 
-def authenticate_user(username: str, password: str):
-    if not os.path.exists(USERS_FILE):
+def authenticate(company_id: str, username: str, password: str):
+    try:
+        df = pd.read_excel(USERS)
+        user = df[
+            (df["company_id"] == company_id) &
+            (df["username"] == username) &
+            (df["password"] == password)
+        ]
+        if user.empty:
+            return None
+        row = user.iloc[0]
+        return {
+            "company_id": row["company_id"],
+            "username": row["username"],
+            "role": row["role"]
+        }
+    except:
         return None
-
-    df = pd.read_excel(USERS_FILE)
-
-    user = df[
-        (df["username"] == username) &
-        (df["password"] == password) &
-        (df["active"] == True)
-    ]
-
-    if user.empty:
-        return None
-
-    row = user.iloc[0]
-    return {
-        "user_id": row["user_id"],
-        "name": row["name"],
-        "role": row["role"]
-    }
