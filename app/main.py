7f9081f14c28f12cd8297501efe263c3fcc01fdb
ovelvoro-stack <ovelvoro@gmail.com>
2026-01-28 17:11:29 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from app.models.api.employee_routes import router as employee_router
 from app.models.api.manager_routes import router as manager_router
 from app.models.api.rules_routes import router as rules_router
+from app.models.api.daily_summary_routes import router as daily_summary_router
 
 app = FastAPI()
 
@@ -13,3 +14,4 @@ def root():
 app.include_router(employee_router)
 app.include_router(manager_router)
 app.include_router(rules_router)
+app.include_router(daily_summary_router)
